5c3874aa93abe1a0d65759f97c984fd8343ade90
Hunter Bertoson <dev@hunterbertoson.tech>
2023-10-26 14:30:12 -0500

    Updated to a custom error handling branch

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,6 +2,8 @@ import {HfInference} from '@huggingface/inference';
 import {HuggingFaceStream, StreamingTextResponse} from 'ai';
 import {experimental_buildOpenAssistantPrompt} from 'ai/prompts';
 
+import {SystemError} from '@/utils/Error';
+
 // Create a new Hugging Face Inference instance
 const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 
@@ -10,25 +12,32 @@ export const runtime = 'edge';
 
 export async function POST(req: Request) {
 	// Extract the `messages` from the body of the request
-	const {messages} = await req.json();
 
-	// Initialize a text-generation stream using the Hugging Face Inference SDK
-	const response = await Hf.textGenerationStream({
-		model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
-		inputs: experimental_buildOpenAssistantPrompt(messages),
-		parameters: {
-			max_new_tokens: 200,
-			// @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
-			typical_p: 0.2,
-			repetition_penalty: 1,
-			truncate: 1000,
-			return_full_text: false,
-		},
-	});
+	try {
+		const {messages} = await req.json();
 
-	// Convert the async generator into a friendly text-stream
-	const stream = HuggingFaceStream(response);
+		// Initialize a text-generation stream using the Hugging Face Inference SDK
+		const response = await Hf.textGenerationStream({
+			model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
+			inputs: experimental_buildOpenAssistantPrompt(messages),
+			parameters: {
+				max_new_tokens: 200,
+				// @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+				typical_p: 0.2,
+				repetition_penalty: 1,
+				truncate: 1000,
+				return_full_text: false,
+			},
+		});
 
-	// Respond with the stream, enabling the client to consume the response
-	return new StreamingTextResponse(stream);
+		// Convert the async generator into a friendly text-stream
+		const stream = HuggingFaceStream(response);
+
+		// Respond with the stream, enabling the client to consume the response
+		return new StreamingTextResponse(stream);
+	} catch (error) {
+		if (error instanceof SystemError) {
+			return new Response(error.message, {status: 500});
+		}
+	}
 }
